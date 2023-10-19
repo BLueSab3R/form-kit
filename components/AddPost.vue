b2650b701bf2ae3962fa0e2904c8e83b2f823b23
BLueSab3R <setunov9@gmail.com>
2023-10-19 20:23:08 +0300

    fixing

--- components/AddPost.vue
+++ components/AddPost.vue
@@ -2,8 +2,10 @@
   <div
     @click="handleBackgroundClick"
     class="fixed flex flex-col items-center justify-center inset-0 bg-black bg-opacity-50"
+  >
+    <div
+      class="bg-white flex flex-col text-black w-1/2 rounded-lg relative max-h-[80vh] overflow-auto"
     >
-    <div class="bg-white flex flex-col text-black w-1/2 rounded-lg relative max-h-[80vh] overflow-auto">
       <h3 class="flex justify-center text-xl">Add new post</h3>
       <button
         @click="$emit('closeAdd')"
@@ -13,234 +15,258 @@
       </button>
 
       <ul class="text-xl mt-7">
-        <li
-          :class="{
-            border: true,
-            'border-red-600 rounded-md': errorsValidation.username,
-            'border-none': !errorsValidation.username,
-          }"
-        >
-          <label class="mr-2" for="username">Username:</label>
-          <input
-            id="username"
-            name="username"
-            v-model="formData.username"
-            type="text"
-            placeholder="new username"
-          />
-        </li>
-        <p class="text-red-600 text-xl" v-if="errorsValidation.username">
-          {{ errorsValidation.username }}
-        </p>
-        <li
-          :class="{
-            border: true,
-            'border-red-600 rounded-md': errorsValidation.name,
-            'border-none': !errorsValidation.name,
-          }"
-        >
-          <label class="mr-2" for="name">Name:</label>
-          <input
-            id="name"
-            name="name"
-            v-model="formData.name"
-            type="text"
-            placeholder="new name"
-          />
-        </li>
-        <p class="text-red-600 text-xl" v-if="errorsValidation.name">
-          {{ errorsValidation.name }}
-        </p>
-         <li
-          :class="{
-            border: true,
-            'border-red-600 rounded-md': errorsValidation.email,
-            'border-none': !errorsValidation.email,
-          }"
-        >
-          <label class="mr-2" for="email">Email:</label>
-          <input
-            id="email"
-            name="email"
-            v-model="formData.email"
-            type="email"
-            placeholder="new email"
-          />
-        </li>
-        <p class="text-red-600 text-xl" v-if="errorsValidation.email">
-          {{ errorsValidation.email }}
-        </p>
-         <li
-          :class="{
-            border: true,
-            'border-red-600 rounded-md': errorsValidation.username,
-            'border-none': !errorsValidation.username,
-          }"
-        >
-          <label class="mr-2" for="website">Website:</label>
-          <input
-            id="website"
-            name="website"
-            v-model="formData.website"
-            type="text"
-            placeholder="new website"
-          />
-        </li>
-        <p class="text-red-600 text-xl" v-if="errorsValidation.website">
-          {{ errorsValidation.website }}
-        </p>
-         <li
-          :class="{
-            border: true,
-            'border-red-600 rounded-md': errorsValidation.city,
-            'border-none': !errorsValidation.city,
-          }"
-        >
-          <label class="mr-2" for="city">City:</label>
-          <input
-            id="city"
-            name="city"
-            v-model="formData.city"
-            type="text"
-            placeholder="new city"
-          />
-        </li>
-        <p class="text-red-600 text-xl" v-if="errorsValidation.city">
-          {{ errorsValidation.city }}
-        </p>
-         <li
-          :class="{
-            border: true,
-            'border-red-600 rounded-md': errorsValidation.phone,
-            'border-none': !errorsValidation.phone,
-          }"
-        >
-          <label class="mr-2" for="phone">Phone:</label>
-          <input
-            id="phone"
-            name="phone"
-            v-model="formData.phone"
-            type="tel"
-            placeholder="new phone"
-          />
-        </li>
-        <p class="text-red-600 text-xl" v-if="errorsValidation.phone">
-          {{ errorsValidation.phone }}
-        </p>
-         <li
-          :class="{
-            border: true,
-            'border-red-600 rounded-md': errorsValidation.street,
-            'border-none': !errorsValidation.street,
-          }"
-        >
-          <label class="mr-2" for="street">Street:</label>
-          <input
-            id="street"
-            name="street"
-            v-model="formData.street"
-            type="text"
-            placeholder="new street"
-          />
-        </li>
-        <p class="text-red-600 text-xl" v-if="errorsValidation.street">
-          {{ errorsValidation.street }}
-        </p>
-         <li
-          :class="{
-            border: true,
-            'border-red-600 rounded-md': errorsValidation.suite,
-            'border-none': !errorsValidation.suite,
-          }"
-        >
-          <label class="mr-2" for="suite">Suite:</label>
-          <input
-            id="suite"
-            name="suite"
-            v-model="formData.suite"
-            type="text"
-            placeholder="new suite"
-          />
-        </li>
-        <p class="text-red-600 text-xl" v-if="errorsValidation.suite">
-          {{ errorsValidation.suite }}
-        </p>
-         <li
-          :class="{
-            border: true,
-            'border-red-600 rounded-md': errorsValidation.zipcode,
-            'border-none': !errorsValidation.zipcode,
-          }"
-        >
-          <label class="mr-2" for="zipcode">Zipcode:</label>
-          <input
-            id="zipcode"
-            name="zipcode"
-            v-model="formData.zipcode"
-            type="value"
-            placeholder="new Zipcode"
-          />
-        </li>
-        <p class="text-red-600 text-xl" v-if="errorsValidation.zipcode">
-          {{ errorsValidation.zipcode }}
-        </p>
-         <li
-          :class="{
-            border: true,
-            'border-red-600 rounded-md': errorsValidation.company_name,
-            'border-none': !errorsValidation.company_name,
-          }"
-        >
-          <label class="mr-2" for="company_name">Company name:</label>
-          <input
-            id="company_name"
-            name="company_name"
-            v-model="formData.company_name"
-            type="text"
-            placeholder="new Company name"
-          />
-        </li>
-        <p class="text-red-600 text-xl" v-if="errorsValidation.company_name">
-          {{ errorsValidation.company_name }}
-        </p>
-         <li
-          :class="{
-            border: true,
-            'border-red-600 rounded-md': errorsValidation.bs,
-            'border-none': !errorsValidation.bs,
-          }"
-        >
-          <label class="mr-2" for="bs">Bs:</label>
-          <input
-            id="bs"
-            name="bs"
-            v-model="formData.bs"
-            type="text"
-            placeholder="new bs"
-          />
-        </li>
-        <p class="text-red-600 text-xl" v-if="errorsValidation.bs">
-          {{ errorsValidation.bs }}
-        </p>
-         <li
-          :class="{
-            border: true,
-            'border-red-600 rounded-md': errorsValidation.catchPhrase,
-            'border-none': !errorsValidation.catchPhrase,
-          }"
-        >
-          <label class="mr-2" for="catchPhrase">Catch phrase:</label>
-          <input
-            id="catchPhrase"
-            name="catchPhrase"
-            v-model="formData.catchPhrase"
-            type="text"
-            placeholder="new catch phrase"
-          />
-        </li>
-        <p class="text-red-600 text-xl" v-if="errorsValidation.catchPhrase">
-          {{ errorsValidation.catchPhrase }}
-        </p>
+        <div>
+          <li
+            :class="{
+              border: true,
+              'border-red-600 rounded-md': errorsValidation.username,
+              'border-none': !errorsValidation.username,
+            }"
+          >
+            <label class="mr-2" for="username">Username:</label>
+            <input
+              id="username"
+              name="username"
+              v-model="formData.username"
+              type="text"
+              placeholder="new username"
+            />
+          </li>
+          <p class="text-red-600 text-xl" v-if="errorsValidation.username">
+            {{ errorsValidation.username }}
+          </p>
+        </div>
+        <div>
+          <li
+            :class="{
+              border: true,
+              'border-red-600 rounded-md': errorsValidation.name,
+              'border-none': !errorsValidation.name,
+            }"
+          >
+            <label class="mr-2" for="name">Name:</label>
+            <input
+              id="name"
+              name="name"
+              v-model="formData.name"
+              type="text"
+              placeholder="new name"
+            />
+          </li>
+          <p class="text-red-600 text-xl" v-if="errorsValidation.name">
+            {{ errorsValidation.name }}
+          </p>
+        </div>
+        <div>
+          <li
+            :class="{
+              border: true,
+              'border-red-600 rounded-md': errorsValidation.email,
+              'border-none': !errorsValidation.email,
+            }"
+          >
+            <label class="mr-2" for="email">Email:</label>
+            <input
+              id="email"
+              name="email"
+              v-model="formData.email"
+              type="email"
+              placeholder="new email"
+            />
+          </li>
+          <p class="text-red-600 text-xl" v-if="errorsValidation.email">
+            {{ errorsValidation.email }}
+          </p>
+        </div>
+        <div>
+          <li
+            :class="{
+              border: true,
+              'border-red-600 rounded-md': errorsValidation.username,
+              'border-none': !errorsValidation.username,
+            }"
+          >
+            <label class="mr-2" for="website">Website:</label>
+            <input
+              id="website"
+              name="website"
+              v-model="formData.website"
+              type="text"
+              placeholder="new website"
+            />
+          </li>
+          <p class="text-red-600 text-xl" v-if="errorsValidation.website">
+            {{ errorsValidation.website }}
+          </p>
+        </div>
+        <div>
+          <li
+            :class="{
+              border: true,
+              'border-red-600 rounded-md': errorsValidation.city,
+              'border-none': !errorsValidation.city,
+            }"
+          >
+            <label class="mr-2" for="city">City:</label>
+            <input
+              id="city"
+              name="city"
+              v-model="formData.city"
+              type="text"
+              placeholder="new city"
+            />
+          </li>
+          <p class="text-red-600 text-xl" v-if="errorsValidation.city">
+            {{ errorsValidation.city }}
+          </p>
+        </div>
+        <div>
+          <li
+            :class="{
+              border: true,
+              'border-red-600 rounded-md': errorsValidation.phone,
+              'border-none': !errorsValidation.phone,
+            }"
+          >
+            <label class="mr-2" for="phone">Phone:</label>
+            <input
+              id="phone"
+              name="phone"
+              v-model="formData.phone"
+              type="tel"
+              placeholder="new phone"
+            />
+          </li>
+          <p class="text-red-600 text-xl" v-if="errorsValidation.phone">
+            {{ errorsValidation.phone }}
+          </p>
+        </div>
+        <div>
+          <li
+            :class="{
+              border: true,
+              'border-red-600 rounded-md': errorsValidation.street,
+              'border-none': !errorsValidation.street,
+            }"
+          >
+            <label class="mr-2" for="street">Street:</label>
+            <input
+              id="street"
+              name="street"
+              v-model="formData.street"
+              type="text"
+              placeholder="new street"
+            />
+          </li>
+          <p class="text-red-600 text-xl" v-if="errorsValidation.street">
+            {{ errorsValidation.street }}
+          </p>
+        </div>
+        <div>
+          <li
+            :class="{
+              border: true,
+              'border-red-600 rounded-md': errorsValidation.suite,
+              'border-none': !errorsValidation.suite,
+            }"
+          >
+            <label class="mr-2" for="suite">Suite:</label>
+            <input
+              id="suite"
+              name="suite"
+              v-model="formData.suite"
+              type="text"
+              placeholder="new suite"
+            />
+          </li>
+          <p class="text-red-600 text-xl" v-if="errorsValidation.suite">
+            {{ errorsValidation.suite }}
+          </p>
+        </div>
+        <div>
+          <li
+            :class="{
+              border: true,
+              'border-red-600 rounded-md': errorsValidation.zipcode,
+              'border-none': !errorsValidation.zipcode,
+            }"
+          >
+            <label class="mr-2" for="zipcode">Zipcode:</label>
+            <input
+              id="zipcode"
+              name="zipcode"
+              v-model="formData.zipcode"
+              type="value"
+              placeholder="new Zipcode"
+            />
+          </li>
+          <p class="text-red-600 text-xl" v-if="errorsValidation.zipcode">
+            {{ errorsValidation.zipcode }}
+          </p>
+        </div>
+        <div>
+          <li
+            :class="{
+              border: true,
+              'border-red-600 rounded-md': errorsValidation.company_name,
+              'border-none': !errorsValidation.company_name,
+            }"
+          >
+            <label class="mr-2" for="company_name">Company name:</label>
+            <input
+              id="company_name"
+              name="company_name"
+              v-model="formData.company_name"
+              type="text"
+              placeholder="new Company name"
+            />
+          </li>
+          <p class="text-red-600 text-xl" v-if="errorsValidation.company_name">
+            {{ errorsValidation.company_name }}
+          </p>
+        </div>
+        <div>
+          <li
+            :class="{
+              border: true,
+              'border-red-600 rounded-md': errorsValidation.bs,
+              'border-none': !errorsValidation.bs,
+            }"
+          >
+            <label class="mr-2" for="bs">Bs:</label>
+            <input
+              id="bs"
+              name="bs"
+              v-model="formData.bs"
+              type="text"
+              placeholder="new bs"
+            />
+          </li>
+          <p class="text-red-600 text-xl" v-if="errorsValidation.bs">
+            {{ errorsValidation.bs }}
+          </p>
+        </div>
+        <div>
+          <li
+            :class="{
+              border: true,
+              'border-red-600 rounded-md': errorsValidation.catchPhrase,
+              'border-none': !errorsValidation.catchPhrase,
+            }"
+          >
+            <label class="mr-2" for="catchPhrase">Catch phrase:</label>
+            <input
+              id="catchPhrase"
+              name="catchPhrase"
+              v-model="formData.catchPhrase"
+              type="text"
+              placeholder="new catch phrase"
+            />
+          </li>
+          <p class="text-red-600 text-xl" v-if="errorsValidation.catchPhrase">
+            {{ errorsValidation.catchPhrase }}
+          </p>
+        </div>
       </ul>
       <button @click="confirmAdd" class="p-10">Confirm</button>
     </div>
@@ -268,14 +294,14 @@ const formData = ref({
 const errorsValidation = ref({});
 const validatePost = object({
   username: string().required(),
-  name:string().required(),
+  name: string().required(),
   email: string().email().required(),
   website: string().url().required(),
   city: string().required(),
   phone: number()
     .positive()
     .min(12, "must in international format, at should be 12 numbers long"),
-  street: string().min(2,'should be at least 3'),
+  street: string().min(2, "should be at least 3"),
   suite: string(),
   zipcode: number().min(5, "index should contain 5 numbers"),
   company_name: string(),
@@ -300,7 +326,7 @@ const confirmAdd = async () => {
     if (error.inner) {
       error.inner.forEach((validateError) => {
         errorsValidation.value[validateError.path] = validateError.message;
-        console.log(validateError.message)
+        console.log(validateError.message);
       });
     }
   }
